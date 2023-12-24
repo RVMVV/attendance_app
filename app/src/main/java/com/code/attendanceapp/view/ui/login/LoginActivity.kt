bbd4e806e7c011f54df822241b7712afc7b87da0
LaluBRamadhan <lalubagoes2@gmail.com>
2023-12-24 12:55:16 +0700

    auth
    adding login and register activity

---
+++ app/src/main/java/com/code/attendanceapp/view/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.code.attendanceapp.view.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.code.attendanceapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

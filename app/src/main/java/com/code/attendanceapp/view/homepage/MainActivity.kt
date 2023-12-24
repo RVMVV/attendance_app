536a6e8c51e15b8004c7f9240b3dbe9974c71dae
LaluBRamadhan <lalubagoes2@gmail.com>
2023-12-24 12:31:48 +0700

    MVVM
    Implement MVVM architecture

---
+++ app/src/main/java/com/code/attendanceapp/view/homepage/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.code.attendanceapp.view.homepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.code.attendanceapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/code/attendanceapp/view/presensi/PresensiActivity.kt
@@ -0,0 +1,12 @@
+package com.code.attendanceapp.view.presensi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.code.attendanceapp.R
+
+class PresensiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_presensi)
+    }
+}
